61a658b313f6acc45cf209158eb3371e23576251
m15129 <mashaowei@h3c.com>
2018-04-27 09:18:16 +0800

    04-26

---
+++ 重庆二师_Dev/doc/ods-fdm-xg_xsxxb.sql
@@ -0,0 +1,30 @@
+
+TRUNCATE talbe fdm.edu_fdm_xs_jbxx
+INSERT INTO fdm.edu_fdm_xs_jbxx SELECT
+	xh,
+	xm,
+	NULL,
+	xmpy,
+	cym,
+	xb,
+	csrq,
+	jg,
+	jg,
+	mz,
+	hkszd,
+	NULL,
+	sfzh,
+	sfjh,
+	NULL,
+	zzmm,
+	jkzk,
+	zjdm,
+	xx,
+	NULL,
+	NULL,
+	NULL,
+	sfzx,
+	NULL,
+	NULL
+FROM
+	ods.xg_XSXXB
